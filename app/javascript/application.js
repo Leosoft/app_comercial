6964ac8d0dd5bde67f03c0c1cf4f4d571e279d67
Leosoft <leosoft3031@gmail.com>
2022-05-19 12:25:17 -0300

    Template Added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+//= require "jquery"
+//= require "popper"
+//= require "boostrap"
+//= require "sb-admin-2.min"
+
+
+import "boostrap";
+import 'font-awesome-webfont';
+import 'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i';
